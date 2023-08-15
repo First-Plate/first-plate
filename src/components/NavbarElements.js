e7e3d75751373fb9aa3595ff1fc7839ad2d3adb6
Tracy Wei <tracywei27@gmail.com>
2023-08-14 22:36:38 -0700

    finished rough ver of home

--- src/components/NavbarElements.js
+++ src/components/NavbarElements.js
@@ -2,71 +2,60 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: white;
-height: 85px;
-display: flex;
-justify-content: space-between;
-padding: 0.2rem calc((100vw - 1000px) / 2);
-z-index: 12;
-/* Third Nav */
-/* justify-content: flex-start; */
+  background: white;
+  height: 85px;
+  display: flex;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
 `;
 
 export const NavLink = styled(Link)`
-color: #808080;
-display: flex;
-align-items: center;
-text-decoration: none;
-padding: 0 1rem;
-height: 100%;
-cursor: pointer;
-&.active {
-	color: #000000;
-}
+  color: #808080;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &.active {
+    color: #000000;
+  }
 `;
 
-
 export const NavMenu = styled.div`
-display: flex;
-align-items: right;
-margin-right: -24px;
-/* Second Nav */
-/* margin-right: 24px; */
-/* Third Nav */
-/* width: 100vw;
-white-space: nowrap; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  white-space: nowrap;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const NavBtn = styled.nav`
-display: flex;
-align-items: center;
-margin-right: 24px;
-/* Third Nav */
-/* justify-content: flex-end;
-width: 100vw; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const NavBtnLink = styled(Link)`
-border-radius: 4px;
-background: black;
-padding: 10px 22px;
-color: white;
-outline: none;
-border: none;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
-/* Second Nav */
-margin-left: 24px;
-&:hover {
-	transition: all 0.2s ease-in-out;
-	background: #fff;
-	color: #808080;
-}
+  border-radius: 4px;
+  background: black;
+  padding: 10px 22px;
+  color: white;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  margin-left: 24px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
+  }
 `;
+
---
+++ src/faqdata.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+const data = {
+    title: "FAQ (How it works)",
+    rows: [
+        {
+            title: "How long is each round?",
+            content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
+              ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
+              In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
+              Fusce sed commodo purus, at tempus turpis.`,
+        },
+        {
+            title: "When is the next drop?",
+            content:
+                "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+        },
+        {
+            title: "How does first date make their matches?",
+            content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
+            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
+            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
+            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+        },
+        {
+            title: "How does first date prioritize safety?",
+            content: <p>current version is 1.2.1</p>,
+        },
+    ],
+};
+
+export default data;
